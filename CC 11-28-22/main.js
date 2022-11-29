6e919f65524ba747e92d967be76234794821edad
mendozar7967 <mendozar7967@gmail.com>
2022-11-28 22:07:54 -0600

    Coding challenge 'MakeUpperCase' and solution 11-28-22

---
+++ CC 11-28-22/main.js
@@ -0,0 +1,10 @@
+//Write a function which converts the input string to uppercase
+
+//code:
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
+
+//test:
+return makeUpperCase("hello")
+//"HELLO"
